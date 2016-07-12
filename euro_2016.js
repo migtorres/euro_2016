4a97c486597dff4748de0db26ca44a9fa8315891
migtorres <migtorres@gmail.com>
2016-07-13 00:09:13 +0100

    circle working

--- euro_2016.js
+++ euro_2016.js
@@ -1,30 +1,64 @@
-var diameter = 960,
-    radius = diameter / 2,
-    innerRadius = radius - 120;
+    var diameter = 500;
+    var radius = diameter / 2;
+    var margin = 20;
+
+
+    function addTooltip(circle) {
+      var x = parseFloat(circle.attr("cx"));
+      var y = parseFloat(circle.attr("cy"));
+      var r = parseFloat(circle.attr("r"));
+      var text = circle.attr("id");
+
+      var tooltip = d3.select("#plot")
+        .append("text")
+        .text(text)
+        .attr("x", x)
+        .attr("y", y)
+        .attr("dy", -r * 2)
+        .attr("id", "tooltip");
+
+      var offset = tooltip.node().getBBox().width / 2;
+
+      if ((x - offset) < -radius) {
+        tooltip.attr("text-anchor", "start");
+        tooltip.attr("dx", -r);
+      } else if ((x + offset) > (radius)) {
+        tooltip.attr("text-anchor", "end");
+        tooltip.attr("dx", r);
+      } else {
+        tooltip.attr("text-anchor", "middle");
+        tooltip.attr("dx", 0);
+      }
+    }
 
 
 function draw (data) {
 
-  var svg = d3.select("body").append("svg")
-    .attr("width", diameter)
-    .attr("height", diameter)
-    .append("g")
-    .attr("transform", "translate(" + radius + "," + radius + ")");
-
-
-    var svg  = d3.select("body").select("#circle")
+      d3.select("body").select("#circle")
         .append("svg")
         .attr("width", diameter)
         .attr("height", diameter);
 
-    var plot = svg.append("g")
+      // draw border around svg image
+      // svg.append("rect")
+      //     .attr("class", "outline")
+      //     .attr("width", diameter)
+      //     .attr("height", diameter);
+
+      // create plot area within svg image
+      d3.select("svg")
+      	.append("g")
         .attr("id", "plot")
         .attr("transform", "translate(" + radius + ", " + radius + ")");
 
-
+      // draw border around plot area
+      // plot.append("circle")
+      //     .attr("class", "outline")
+      //     .attr("r", radius - margin);
 
  	var nodes = filterTeams(data)
-  circleLayout(nodes)
+  	circleLayout(nodes)
+  	drawNodes(nodes);
   
   function circleLayout(nodes) {
 
@@ -37,13 +71,39 @@ function draw (data) {
       nodes.forEach(function(d, i) {
         // calculate polar coordinates
         var theta = scale(i);
-        var radial = innerRadius;
+        var radial = radius-margin;
 
         // convert to cartesian coordinates
         d.x = radial * Math.sin(theta);
         d.y = radial * Math.cos(theta);
       });
 
+    }
+
+    function drawNodes(nodes) {
+      // used to assign nodes color by group
+      d3.select("#plot").selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("circle")
+        .attr("class", "node")
+        .attr("id", function(d, i) {
+          return d.name;
+        })
+        .attr("cx", function(d, i) {
+          return d.x;
+        })
+        .attr("cy", function(d, i) {
+          return d.y;
+        })
+        .attr("r", 5)
+        .style("fill", "#black")
+        .on("mouseover", function(d, i) {
+          addTooltip(d3.select(this));
+        })
+        .on("mouseout", function(d, i) {
+          d3.select("#tooltip").remove();
+        });
     }
 
  	function filterTeams (data){
@@ -69,6 +129,7 @@ function draw (data) {
 
   return team_list;
   }
+
 
 
 }
